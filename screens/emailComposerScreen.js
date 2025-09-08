a0952becf6a1c18b5cbfaa67c0f09017b42f71fe
analu65 <analuciacesario910@gmail.com>
2025-09-08 17:29:32 -0300

    continua imagem

--- screens/emailComposerScreen.js
+++ screens/emailComposerScreen.js
@@ -46,7 +46,35 @@ const EmailComposerScreen = () => {
     };
 
     const selecionarImagem = async () => {
-      
+      try {
+        const {status} = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+        if (status !== 'granted') {
+          Alert.alert('Permissão necessária', 'Precisamos de acesso à sua galeria.');
+          return;
+        }
+        const result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          allowsEditing: true,
+          aspect: [4,3],
+          quality: 0.8,
+        });
+
+        if (!result.canceled) {
+          setImagem(result.assets[0]);
+        } catch (error){
+          console.error('Erro ao selecionar imagem:', error);
+          Alert.alert('Erro', 'Não foi possível selecionar a imagem');
+        }
+      };
+      const fazerUploadImagem = async () => {
+        if (!imagem) return null;
+        setUploading(true);
+
+        try {
+          const response = 
+        }
+      }
     }
     
     const enviarEmails = async () => {
